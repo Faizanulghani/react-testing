ef790dbc751f9bcf1610ace11b9b8ab3717227b4
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-09-05 18:39:14 +0500

    beforeEach | beforeAll | afterAll | afterEach hooks

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,39 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("Click Event Test", () => {
+// beforeAll(() => {
+//   console.log("Before All");
+// });
+// beforeEach(() => {
+//   console.log("Before Each");
+// });
+
+test("Click Event Test1", () => {
+  console.log("1");
   render(<App />);
   let btn = screen.getByRole("button");
   fireEvent.click(btn);
   expect(screen.getByText("Hello World")).toBeInTheDocument();
 });
+test("Click Event Test2", () => {
+  console.log("2");
+  render(<App />);
+  let btn = screen.getByRole("button");
+  fireEvent.click(btn);
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
+});
+test("Click Event Test3", () => {
+  console.log("3");
+  render(<App />);
+  let btn = screen.getByRole("button");
+  fireEvent.click(btn);
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
+});
+
+// afterAll(()=>{
+//   console.log("After All");
+// })
+
+afterEach(()=>{
+  console.log("After Each");
+})
