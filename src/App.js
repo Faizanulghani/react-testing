f7e73f3a3c6891e1453ac44d275b7d5ac66f7b7c
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-09-08 19:47:30 +0500

    TextMatch with Function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>RTL Query : getByText and getAllByText</h1>
-      <button>Login</button>
-      <p className="para">This is a paragraph</p>
-      <h1>This is a Heading</h1>
-      <h1>This is a Heading</h1>
+      <h1>TextMatch with Function</h1>
+      <div>Hello World</div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,29 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("button test case", () => {
+test("test match with function", () => {
   render(<App />);
-  let btn = screen.getByText("Login");
-  expect(btn).toBeInTheDocument();
-});
-
-test("p tag test case", () => {
-  render(<App />);
-  let pTag = screen.getByText("This is a paragraph");
-  expect(pTag).toBeInTheDocument();
-  expect(pTag).toHaveClass("para");
-});
-
-// test("h1 tag test case", () => {
-//   render(<App />);
-//   let hTag = screen.getByText("This is a Heading")
-//   expect(hTag).toBeInTheDocument();
-// });
-
-test("multiple h1 tag test case", () => {
-  render(<App />);
-  let hTag = screen.getAllByText("This is a Heading");
-  for (let i = 0; i < hTag.length; i++) {
-    expect(hTag[i]).toBeInTheDocument();
-  }
+  const div = screen.getByText((content, element) =>
+    content.startsWith("Hello")
+  );
+  expect(div).toBeInTheDocument();
 });
