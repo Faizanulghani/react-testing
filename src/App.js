20e0ff45e3b43154eee02e3f284c550542936556
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-09-08 14:01:20 +0500

    Multiple Elements and custom role

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ function App() {
   return (
     <div className="App">
       <h1>getByRole</h1>
-      <input type="text" defaultValue={"hello"} disabled />
-      <button>CLick Me!</button>
+      <button>CLick Me 1</button>
+      <button>CLick Me 2</button>
+      <label htmlFor="user">User Name</label>
+      <input type="text" id="user" />
+      <label htmlFor="age">User Age</label>
+      <input type="text" id="age" />
+      <div role="dummy">Dummy Text</div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,17 @@ import App from "./App";
 
 test("get by role", () => {
   render(<App />);
-  const input = screen.getByRole("textbox");
-  const btn = screen.getByRole("button");
-  expect(input).toBeInTheDocument();
-  expect(input).toHaveValue("hello");
-  expect(input).toBeDisabled();
-  expect(btn).toBeInTheDocument();
+  const btn1 = screen.getByRole("button", { name: "CLick Me 1" });
+  const btn2 = screen.getByRole("button", { name: "CLick Me 2" });
+  const input1 = screen.getByRole("textbox", { name: "User Name" });
+  const input2 = screen.getByRole("textbox", { name: "User Age" });
+  const div = screen.getByRole("dummy")
+  expect(div).toBeInTheDocument();
+
+
+
+  expect(btn1).toBeInTheDocument();
+  expect(btn2).toBeInTheDocument();
+  expect(input1).toBeInTheDocument();
+  expect(input2).toBeInTheDocument();
 });
