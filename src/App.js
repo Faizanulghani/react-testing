79261058caec4629705d19eb2b82a11ec5f54b64
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-09-08 15:28:10 +0500

    getByPlaceholderText and getAllByPlaceholderText

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>RTL Query : getAllByLabelText</h1>
+      <h1>RTL Query : ByPlaceholderText</h1>
       <label htmlFor="username">Username</label>
-      <input type="text" id="username" />
+      <input type="text" id="username" placeholder="Enter Username" />
 
       <label htmlFor="username2">Username</label>
-      <input type="text" id="username2" />
+      <input type="text" id="username2" placeholder="Enter Username" />
 
       <label htmlFor="username3">Username</label>
-      <input type="text" id="username3" />
+      <input type="text" id="username3" placeholder="Enter Username" />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from "./App";
 
 test("input test case", () => {
   render(<App />);
-  const inputs = screen.getAllByLabelText("Username");
+  const inputs = screen.getAllByPlaceholderText("Enter Username");
 
   for (let i = 0; i < inputs.length; i++) {
     expect(inputs[i]).toBeInTheDocument();
